b187302e5548e04c64668d6298e82215a4f2d302
Meuss <thomas.miller147@gmail.com>
2020-12-02 14:00:53 +0100

    9. Store Model

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,6 +3,6 @@ const { route } = require("../app");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
 
-router.get("/", storeController.myMiddleware, storeController.homePage);
+router.get("/", storeController.homePage);
 
 module.exports = router;
