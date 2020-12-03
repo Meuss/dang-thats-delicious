a369f74bf61001a400929e3f27f3492861300500
Meuss <thomas.miller147@gmail.com>
2020-12-03 11:30:59 +0100

    20. unique store slugs

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -38,12 +38,19 @@ const storeSchema = new mongoose.Schema({
 });
 
 // Auto-generate the slug when saving a store
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of xxx, xxx-1, xxx-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+
   next();
   // TODO make more resiliant so slugs are unique
 });
